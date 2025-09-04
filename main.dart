2da7200292eb725a284402867e267bb4b45216d7
Leonardo Arruda <la000476@gmail.com>
2025-09-04 00:00:37 +0000

    feat: if statement

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+void main(){
+    
+    /*print('Informe sua idade: ');
+    String? nome = stdin.readLineSync();*/
+
+    int idade = 19;
+
+/*    if(idade >= 18)
+    {
+        print('pode entrar na balada');
+    }
+        print('fim do programa');
+}*/
+    if(idade < 18)
+    {
+        print('fim do programa');
+        return;
+    }
+        print('pode entrar na balada');
+}
