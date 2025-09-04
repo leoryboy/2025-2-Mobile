84a8a06fa3a6f27de0077d9bb9863ee25f21c174
Leonardo Arruda <la000476@gmail.com>
2025-09-04 00:49:31 +0000

    feat: comando else if

--- main.dart
+++ main.dart
@@ -1,10 +1,33 @@
 import 'dart:io';
 void main(){
+    int renda = 60000;
+    int saldo = 100000;
+
+    if(renda >= 1400 && renda < 5000){
+        print('Varejo');
+    }else if(renda >= 5000 && renda < 30000){
+        print('Vangogh');
+    }else if(renda >= 30000 && renda < 60000){
+        print('Select');
+    }else if( renda >= 60000 && saldo >= 100000){
+        print('Exclusive');
+    }
+
+}
+
+
+
+
+
+
+
+
+
     
     /*print('Informe sua idade: ');
     String? nome = stdin.readLineSync();*/
 
-    int idade = 19;
+  //  int idade = 19;
 
 /*    if(idade >= 18)
     {
@@ -12,10 +35,10 @@ void main(){
     }
         print('fim do programa');
 }*/
-    if(idade < 18)
+/*   if(idade < 18)
     {
         print('fim do programa');
         return;
     }
         print('pode entrar na balada');
-}
+}*/
