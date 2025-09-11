c3918e233413bfb0f6792b853196bae83aadbd94
Leonardo Arruda <la000476@gmail.com>
2025-09-11 19:38:35 +0000

    Exercício 3: Classificador de IMC (Índice de Massa Corporal)

---
+++ exercicios/ex03.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+void main() {
+
+  print("Digite seu peso em kg:");
+  String? entradaPeso = stdin.readLineSync();
+  double peso = double.parse(entradaPeso!);
+
+
+  print("Digite sua altura em metros (ex: 1.75):");
+  String? entradaAltura = stdin.readLineSync();
+  double altura = double.parse(entradaAltura!);
+
+  // Calcula o IMC
+  double imc = peso / (altura * altura);
+
+  String classificacao;
+  if (imc < 18.5) {
+    classificacao = "Abaixo do peso";
+  } else if (imc < 25) {
+    classificacao = "Peso normal";
+  } else if (imc < 30) {
+    classificacao = "Sobrepeso";
+  } else if (imc < 35) {
+    classificacao = "Obesidade grau 1";
+  } else if (imc < 40) {
+    classificacao = "Obesidade grau 2";
+  } else {
+    classificacao = "Obesidade grau 3";
+  }
+
+  print("Seu IMC é ${imc.toStringAsFixed(2)} - $classificacao");
+}
